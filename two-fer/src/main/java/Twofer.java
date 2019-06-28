c69de23fecdbd2755f20b75c39f9688c138149dc
Kelly Farlow <kelly.farlow@gmail.com>
2019-06-29 00:42:29 +1000

    Two Fer: Use ternary operator

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,9 +1,5 @@
 class Twofer {
     String twofer(String name) {
-        String who = name;
-        if (who == null) {
-          who = "you";
-        }
-        return "One for " + who + ", one for me.";
+        return "One for " + (name == null ? "you" : name) + ", one for me.";
     }
 }
