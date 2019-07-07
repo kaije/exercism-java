f069512e9447d8ea31b65901e2e15e8d52563bb9
Kelly Farlow <kelly.farlow@gmail.com>
2019-07-07 10:01:40 +1000

    Hamming Distance: Extract calcuation into separate method and remove redundant checks in validateInput.

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -15,9 +15,7 @@ class Hamming {
 
   int getHammingDistance() {
     if (this.hammingDistance < 0) {
-      this.hammingDistance = (int) IntStream.range(0, this.leftStrand.length())
-      .filter(i -> this.leftStrand.charAt(i) != this.rightStrand.charAt(i))
-      .count();
+      this.hammingDistance = calculateHammingDistance();
     }
     return this.hammingDistance;
   }
@@ -26,12 +24,18 @@ class Hamming {
     if (leftStrand.length() == rightStrand.length()) {
       return;
     }
-    if (leftStrand.isEmpty() && !rightStrand.isEmpty()) {
+    if (leftStrand.isEmpty()) {
       throw new IllegalArgumentException("left strand must not be empty.");
     }
-    if (!leftStrand.isEmpty() && rightStrand.isEmpty()) {
+    if (rightStrand.isEmpty()) {
       throw new IllegalArgumentException("right strand must not be empty.");
     }
     throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+  }
+
+  private int calculateHammingDistance() {
+    return (int) IntStream.range(0, this.leftStrand.length())
+    .filter(i -> this.leftStrand.charAt(i) != this.rightStrand.charAt(i))
+    .count();
   }
 }
